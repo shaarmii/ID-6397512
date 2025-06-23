b5b8712a2050521f9095c32517aefea5b484f036
shaarmii <160403613+shaarmii@users.noreply.github.com>
2025-06-24 05:23:36 +0530

    Delete factoryPatternExample directory

--- factoryPatternExample/Document.java
+++
@@ -1,5 +0,0 @@
-package factoryPatternExample;
-
-public interface Document {
-	void open();
-}
--- factoryPatternExample/DocumentFactory.java
+++
@@ -1,6 +0,0 @@
-package factoryPatternExample;
-
-public abstract class DocumentFactory {
-	public abstract Document createDocument();
-
-}
